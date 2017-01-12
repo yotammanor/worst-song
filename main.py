f62e0bea0f444757e2085679503f024d833ae8b7
Yotam <yotammanor@gmail.com>
2017-01-12 14:38:51 +0200

    add more statuses to random pool

--- main.py
+++ main.py
@@ -9,15 +9,13 @@ import json
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-NUM_OF_STATUSES = 100
-
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 
 @app.route('/get_song/', methods=['GET'])
 def get_song():
-    statuses = status_fetcher.get_statuses_content(NUM_OF_STATUSES)
+    statuses = status_fetcher.get_statuses_content()
     writer = song_writer.SongWriter(material=statuses)
     song = writer.write()
     title = writer.get_song_title()
@@ -29,12 +27,7 @@ def get_song():
 @app.route('/', methods=['GET'])
 def main():
     """Return a friendly HTTP greeting."""
-    statuses = status_fetcher.get_statuses_content(NUM_OF_STATUSES)
-    writer = song_writer.SongWriter(material=statuses)
-    song = writer.write()
-    title = writer.get_song_title()
-    song = song.replace('\n', '<br>')
-    return render_template('main.html', **{'song': song, 'title': title})
+    return render_template('main.html')
     # return u'<p>song:<br>{}</p>'.format(song)
 
 
--- status_fetcher.py
+++ status_fetcher.py
@@ -4,19 +4,21 @@ import json
 
 KIKAR_API_PATH = 'http://kikar.org/api/v1/facebook_status/'
 
+NUM_OF_STATUSES = 100
 
 def get_yesterday():
     return (datetime.datetime.now() - datetime.timedelta(days=1)).strftime(
         '%Y-%m-%d')
 
 
-def request_statuses():
-    url = KIKAR_API_PATH + '?is_current=true&published__gte=' + get_yesterday()
+def request_statuses(num_of_statuses):
+    url = KIKAR_API_PATH + '?limit={}&is_current=true&published__gte='.format(
+        num_of_statuses) + get_yesterday()
     res = urllib2.urlopen(url)
     return json.loads(res.read())
 
 
-def get_statuses_content(num_of_statuses=None):
-    response = request_statuses()
+def get_statuses_content(num_of_statuses=NUM_OF_STATUSES):
+    response = request_statuses(num_of_statuses)
     return [status['content'] for status in
             response['objects'][:num_of_statuses]]
